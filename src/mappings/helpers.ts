09a2ddf4d3c92397cf4d8e625bb3d2067c214a46
The Professor <92867876+zerorekt@users.noreply.github.com>
2023-09-15 09:51:28 +0700

    init

--- src/mappings/helpers.ts
+++ src/mappings/helpers.ts
@@ -7,7 +7,7 @@ import { User, Bundle, Token, LiquidityPosition, LiquidityPositionSnapshot, Pair
 import { Factory as FactoryContract } from '../types/templates/Pair/Factory'
 
 export const ADDRESS_ZERO = '0x0000000000000000000000000000000000000000'
-export const FACTORY_ADDRESS = '0x2900A08e946F2C5b0f53f4f6BC917c1CF35d0322'
+export const FACTORY_ADDRESS = '0x23ed59208412e5968121ef59790449dd85340d85'
 
 export let ZERO_BI = BigInt.fromI32(0)
 export let ONE_BI = BigInt.fromI32(1)
